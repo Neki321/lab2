8df8ae8eff0ae4fb24281fe9d293fe56ab1c0883
Neki321 <vlad.furman.13@gmail.com>
2022-12-13 17:43:50 +0200

    init
    i

---
+++ +-+-/+-+-.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+void main()
+{
+    double a, b, c;
+    char n;
+    cout << "Enter math action " << endl;
+    cin >> n;
+    cout << "Input a " << endl;
+    cin >> a;
+    cout << "Input b " << endl;
+    cin >> b;
+    switch (n)
+    {
+        case '+': 
+        {
+            c = a + b;
+            break; 
+        }
+        case '-':
+        {
+            c = a - b;
+            break; 
+        }
+        case '*':
+        {
+            c = a * b;
+            break;
+        }
+        case '/':
+        {
+            c = a / b;
+            break; 
+        }
+        case '√':
+        {
+            c = sqrt(b);
+            break;
+        }
+                            
+        case '^':
+        {
+            c = pow(a, b);
+            break;
+        }
+        default:
+        {
+            cout << "Error" << endl;
+        }
+       }
+    cout << c;
+}
+
